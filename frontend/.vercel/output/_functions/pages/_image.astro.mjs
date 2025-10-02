170ef37e5d3c96d48e7b7a5569934348816c30f6
kevinagyeman <kevinagyemann@gmail.com>
2025-10-02 16:08:30 +0200

    build:static pages

--- frontend/.vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BSPpa6ZJ.mjs';
-export { renderers } from '../renderers.mjs';
--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
   integrations: [react()],
 });
