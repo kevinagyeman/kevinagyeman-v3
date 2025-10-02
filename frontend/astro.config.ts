90980d26588d23c081728ae4bef2aab24e7703b6
kevinagyeman <kevinagyemann@gmail.com>
2025-10-02 18:46:07 +0200

    build: fix paths

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -4,19 +4,10 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import path from 'path';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        // your aliases should mirror the ones defined in your tsconfig.json
-        // i'll re-use the ones in the official docs:
-        '@components': path.resolve(path.dirname(''), './src/components'),
-        '@assets': path.resolve(path.dirname(''), './src/assets'),
-      },
-    },
   },
   output: 'server',
   adapter: vercel(),
