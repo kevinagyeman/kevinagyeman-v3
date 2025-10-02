a1d0700a48397473c7723cbdc933ce01a4391791
kevinagyeman <kevinagyemann@gmail.com>
2025-10-02 18:08:38 +0200

    build: text remove path for build

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -4,16 +4,10 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import path from 'path';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve('./src'),
-      },
-    },
   },
   output: 'server',
   adapter: vercel(),
