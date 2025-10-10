5bc411be55af546cff06e9cfed788ddd9d4cf417
kevinagyeman <kevinagyemann@gmail.com>
2025-10-10 22:24:34 +0200

    test

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -35,7 +35,7 @@ REST_AUTH = {
     "USE_JWT": True,
     "JWT_AUTH_COOKIE": "access_token",
     "JWT_AUTH_REFRESH_COOKIE": "refresh_token",
-    "JWT_AUTH_HTTPONLY": False,
+    "JWT_AUTH_HTTPONLY": True,
     "JWT_AUTH_SAMESITE": "None",
     "JWT_AUTH_SECURE": True,
 }
