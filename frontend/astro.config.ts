01acc0365cf43f287ac55c13cdd2cc3fe27cd70f
kevinagyeman <kevinagyemann@gmail.com>
2025-10-26 11:24:45 +0100

    build: partial fix static pages

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -1,19 +1,15 @@
 // @ts-check
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  }),
   site: 'https://www.kevinagyeman.com',
   integrations: [react(), sitemap()],
 });
