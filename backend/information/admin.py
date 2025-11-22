fae8c35228b8ddb66e70a1c1d7947bb7cc02b52b
kevinagyeman <kevinagyemann@gmail.com>
2025-11-22 10:59:32 +0100

    feat: django admin dashboard

--- backend/information/admin.py
+++ backend/information/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Information
+
+
+@admin.register(Information)
+class InformationAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "role", "email", "location")
+    search_fields = ("first_name", "last_name", "email", "role")
+
+    fieldsets = (
+        (
+            "Personal Information",
+            {"fields": ("first_name", "last_name", "role", "email", "location")},
+        ),
+        ("Professional", {"fields": ("summary", "about", "skills")}),
+        ("Links & Media", {"fields": ("main_link", "links", "image", "resume")}),
+    )
--- backend/project/admin.py
+++ backend/project/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "start_date",
+        "end_date",
+        "is_published",
+        "is_present_date",
+    )
+    list_filter = ("is_published", "is_present_date", "start_date")
+    search_fields = ("title", "description", "skills")
+    date_hierarchy = "start_date"
+    ordering = ("-start_date",)
+
+    fieldsets = (
+        (
+            "Basic Information",
+            {"fields": ("title", "short_description", "description")},
+        ),
+        ("Timeline", {"fields": ("start_date", "end_date", "is_present_date")}),
+        ("Technical Details", {"fields": ("skills", "links", "image")}),
+        ("Publishing", {"fields": ("is_published",)}),
+    )
+
+    list_editable = ("is_published",)
