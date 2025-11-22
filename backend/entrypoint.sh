002d24c3985e5fdf50975cd2ce2e219ec1c61b23
kevinagyeman <kevinagyemann@gmail.com>
2025-11-22 10:42:55 +0100

    build: cleanup docker files

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -e
+
+echo "Starting Django application..."
+
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
+
+echo "Starting server..."
+
 exec "$@"
