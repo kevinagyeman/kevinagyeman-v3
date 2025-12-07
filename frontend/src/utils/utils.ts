d7a8955693f42350888628c639c625466b87e48e
kevinagyeman <kevinagyemann@gmail.com>
2025-12-07 04:21:21 +0100

    feat: add SEO slug for project single page

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -33,3 +33,12 @@ export function filterInformationtData(data: InformationSchema) {
 
 	return newData;
 }
+
+export function generateSlug(title: string): string {
+	return title
+		.toLowerCase()
+		.replace(/[^\w\s-]/g, "")
+		.replace(/\s+/g, "-")
+		.replace(/-+/g, "-")
+		.trim();
+}
