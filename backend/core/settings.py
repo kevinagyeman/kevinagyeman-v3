9b079cd1c150e60529edf93f956607eb08311092
kevinagyeman <kevinagyemann@gmail.com>
2025-12-13 12:10:29 +0100

    config: extend JWT token lifetimes to improve user experience

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -57,8 +57,8 @@ REST_AUTH = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
